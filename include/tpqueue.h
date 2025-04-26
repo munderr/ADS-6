55a3f49b45f218bd111dd2481ff393372235a683
munderr <mvyazov534@gmail.com>
2025-04-26 11:42:05 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,55 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+private:
+    struct Element {
+        T value;
+        Element* next;
+        Element(const T& val) : value(val), next(nullptr) {}
+    };
+    Element* first;
+public:
+    TPQueue() : first(nullptr) {}
+    ~TPQueue() {
+        while (first) {
+            Element* toDelete = first;
+            first = first->next;
+            delete toDelete;
+        }
+    }
+    void push(const T& item) {
+        Element* newElem = new Element(item);
+        if (!first) {
+            first = newElem;
+            return;
+        }
+        if (item.prior > first->value.prior) {
+            newElem->next = first;
+            first = newElem;
+            return;
+        }
+        Element* current = first;
+        while (current->next && current->next->value.prior >= item.prior) {
+            current = current->next;
+        }
+        newElem->next = current->next;
+        current->next = newElem;
+    }
+    T pop() {
+        if (!first) {
+            throw std::out_of_range("Queue is empty!");
+        }
+        Element* toRemove = first;
+        T tempVal = first->value;
+        first = first->next;
+        delete toRemove;
+        return tempVal;
+    }
 };
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
